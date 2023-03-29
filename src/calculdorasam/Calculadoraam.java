8d04b7acaef3affe71ad1b2482c7a0ecea9e30c5
sam nojoza <nojoza@outlook.com>
2023-03-29 11:28:23 -0300

    Initial commit

---
+++ src/calculdorasam/Calculadoraam.java
@@ -0,0 +1,17 @@
+package calculdorasam;
+/*
+ * Calculadora
+ * 
+ * Versão: 1.0
+ * 
+ * Autor: Samuel Nojoza
+ * 
+ */
+public class Calculadoraam {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
